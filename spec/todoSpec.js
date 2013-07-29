dbdc6cb229f8bae5aa6f45db8e26b0eaed90ca81
Christopher Wright <cmwright33@gmail.com>
2013-07-29 17:41:41 -0400

    can add a task to a todo list, started on testing the completebutton

--- spec/todoSpec.js
+++ spec/todoSpec.js
@@ -8,7 +8,7 @@ describe("a TodoItem Object", function() {
 });
 
 
-describe("creating a TodoItem clone", function() {
+describe("creating a todoItem clone", function() {
 
   it("creates a clone of the Object", function() {
     todo1 = Object.create(todoItem);
--- src/todo.js
+++ src/todo.js
@@ -1,17 +1,37 @@
 var todoApp = {
 
+
+
+//Creates an array to put the todo objects in
+
+  todoItems: [],
+
+//creates a new task
   createTask: function(){
     var newTodoItem = Object.create(todoItem);
-    newTodoItem.task = document.getElementById('new-text-field').value;
-    TodoItems.push(newTodoItem);
-  };
+    this.todo.push(newTodoItem);
+  },
+
+// appends the task to the DOM
+  appendTask: function() {
+      var parent = document.getElementById('todo-items');
+      var tag = document.createElement('li');
+      for (var i = 0; i < todoItems.length; i++) {
+      tag.innerText = this.todos[i].taskText;
+      parent.appendChild(tag);
+    }
+  }
 };
 
 var todoItem = {
-
+  // sets the task text
+  setTaskText: function(taskInput){
+    newTodoItem.taskText = taskInput;
+  }
 
 };
 
+// This creates a clones of the object
 
 if (typeof Object.create !== 'function') {
     Object.create = function (o) {
@@ -25,14 +45,12 @@ if (typeof Object.create !== 'function') {
 newTodoItem = Object.create(todoItem);
 
 
-//Creates an array to put the todo objects in
-
-TodoItems = [];
+var input;
 
 // Loads the window
 window.onload = function(){
 
-//On click adds the text field to a new newTodoItem and pushs it into an arrayg
+// On click adds the text field to a new newTodoItem and pushs it into an arrayg
 document.getElementById('add-item').onclick = function(event) {
   createTask('new-text-field.value');
 };
